800c795059b0cc1dedb93a1238de0aacaa7bb1b7
caffwin <catheralligator@gmail.com>
2022-04-22 20:40:23 -0700

    class implementation for heightmap parser

--- 09_smoke_basin/9_part2.py
+++ 09_smoke_basin/9_part2.py
@@ -2,49 +2,70 @@
 
 TEST_INPUT_FILE = 'puzzle_input.txt'
 
-def parse_input(file_name):
-    heightmap = []
-    with open(file_name) as file:
-        for row in file:
-            stripped_row = row.strip()
-            row_lst = []
-            for column_value in stripped_row:
-                row_lst.append(int(column_value))
-            heightmap.append(row_lst)
-
-    return heightmap
-
-
-def find_low_point_coords(heightmap_matrix):
+class Parser():
     """
-    Takes in a heightmap matrix and returns a list of coordinates of low points (tuples)
+        Processes input into a heightmap
+        Class attributes:
+        input_file (str)
+        heightmap (list of lists)
+
+        Helper functions:
+        Parses input into heightmap
     """
-    low_point_coords = []
 
-    for r in range(len(heightmap_matrix)):
-        row = heightmap_matrix[r]
-        for c in range(len(row)):
-            col_value = row[c]
-            adjacent_coordinate_values = []
-            adjacent_coordinates = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
-            
-            valid_adjacent_coords = []
+    def __init__(self, input_file, heightmap=None):
+        self.input_file = input_file
+        self.heightmap = heightmap
 
-            for coord_pair in adjacent_coordinates:
-                if 0 <= coord_pair[0] < len(heightmap_matrix) and 0 <= coord_pair[1] < len(row):
-                    valid_adjacent_coords.append(coord_pair)
-                    adjacent_coordinate_values.append(heightmap_matrix[coord_pair[0]][coord_pair[1]])
-                  
-            num_valid_coords = 0
+    def heightmap(self):
+        return self.heightmap
 
-            for pair in valid_adjacent_coords:
-                if col_value < heightmap_matrix[pair[0]][pair[1]]:
-                    num_valid_coords += 1
+    def parse_input_file(self):
+        """
+        Takes in an input file as a string and produces a heightmap matrix (list of lists) and
+        assigns to self.heightmap
+        """
 
-            if num_valid_coords == len(valid_adjacent_coords):
-                low_point_coords.append((r, c))
+        self.heightmap = []
+        with open(self.input_file) as file:
+            for row in file:
+                stripped_row = row.strip()
+                row_lst = []
+                for column_value in stripped_row:
+                    row_lst.append(int(column_value))
+                self.heightmap.append(row_lst)
+
         
-    return low_point_coords
+    def find_low_point_coords(self):
+        """
+        Returns a list of coordinates of low points (tuples) from a heightmap matrix.
+        """
+        low_point_coords = []
+
+        for r in range(len(self.heightmap)):
+            row = self.heightmap[r]
+            for c in range(len(row)):
+                col_value = row[c]
+                adjacent_coordinate_values = []
+                adjacent_coordinates = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+                
+                valid_adjacent_coords = []
+
+                for coord_pair in adjacent_coordinates:
+                    if 0 <= coord_pair[0] < len(self.heightmap) and 0 <= coord_pair[1] < len(row):
+                        valid_adjacent_coords.append(coord_pair)
+                        adjacent_coordinate_values.append(self.heightmap[coord_pair[0]][coord_pair[1]])
+                    
+                num_valid_coords = 0
+
+                for pair in valid_adjacent_coords:
+                    if col_value < self.heightmap[pair[0]][pair[1]]:
+                        num_valid_coords += 1
+
+                if num_valid_coords == len(valid_adjacent_coords):
+                    low_point_coords.append((r, c))
+            
+        return low_point_coords
 
 
 def calc_basin_size_recursive(heightmap_matrix, current_coord, visited_set):
@@ -75,21 +96,24 @@ def calc_basin_size_recursive(heightmap_matrix, current_coord, visited_set):
 
     return basin_sum
 
-def calc_sum_basin_size(heightmap_matrix):
+def calc_product_basin_size():
     visited_set = set()
-    low_point_coords = find_low_point_coords(heightmap_matrix)
+    parser = Parser(TEST_INPUT_FILE)
+    parser.parse_input_file()
+    low_point_coords = parser.find_low_point_coords()
+
     basin_size_list = []
 
     for low_point_coord in low_point_coords:
-        basin_size_list.append(calc_basin_size_recursive(heightmap_matrix, low_point_coord, visited_set))
+        basin_size_list.append(calc_basin_size_recursive(parser.heightmap, low_point_coord, visited_set))
 
     sorted_basin_size_list = sorted(basin_size_list)
     sum_basin_sizes = sorted_basin_size_list[-1] * sorted_basin_size_list[-2] * sorted_basin_size_list[-3]
+
     return sum_basin_sizes
 
 def main():
-    heightmap = parse_input(TEST_INPUT_FILE)
-    print(calc_sum_basin_size(heightmap))
+    print(calc_product_basin_size())
 
 if __name__ == "__main__":
     main()
