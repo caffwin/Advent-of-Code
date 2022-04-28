affd20235d294f8ed29388e66d3f2de858468aa5
caffwin <catheralligator@gmail.com>
2022-04-27 21:40:18 -0700

    apply code changes, need to revisit visited_set comment

--- 09_smoke_basin/9_p1_p2.py
+++ 09_smoke_basin/9_p1_p2.py
@@ -5,40 +5,25 @@ INPUT_FILENAME = 'puzzle_input.txt'
 
 class HeightMapAnalyzer():
     """
-        Parses input into a heightmap
-        Class attributes:
-        input_file (str)
-        heightmap (list of lists)
+    Finds low points in heightmap, and uses them to:
+    CalcPartOneSolution - calculate risk level using sum of each low point + 1
+    CalcPartTwoSolution - recursively calculate basin size
+    
+    Additional helper functions calculate risk level and basin size to find the solution for parts 1 and 2.
+
+    Class attributes:
+    input_file (str)
+    heightmap (list of lists)
     """
 
-    def __init__(self, input_file, heightmap=None):
-        self.input_file = input_file
+    def __init__(self, heightmap=None):
         self.heightmap = heightmap
 
-    def heightmap(self):
-        return self.heightmap
-
     """
-        Helper functions:
+    Public functions:
     """
-
-    def ParseInputFile(self):
-        """
-        Takes in an input file as a string and produces a heightmap matrix (list of lists) and
-        assigns to self.heightmap
-        """
-
-        self.heightmap = []
-        with open(self.input_file) as file:
-            for row in file:
-                stripped_row = row.strip()
-                row_lst = []
-                for column_value in stripped_row:
-                    row_lst.append(int(column_value))
-                self.heightmap.append(row_lst)
-
         
-    def ComputeLowPoints(self):
+    def CalcLowPoints(self):
         """
         Returns a list of coordinates of low points (tuples) from a heightmap matrix.
         """
@@ -69,29 +54,24 @@ class HeightMapAnalyzer():
             
         return low_point_coords
 
-    def CalcRiskLevel(self, low_point_list):
+    def CalcPartOneSolution(self, low_points):
         """
-        Risk level is the sum of each low points + 1
+        Risk level is the sum of each (low point + 1).
         """
 
-        risk_level_sum = len(low_point_list)
+        risk_level_sum = 0
 
-        for low_point in low_point_list:
-            risk_level_sum += self.heightmap[low_point[0]][low_point[1]]
+        for low_point in low_points:
+            risk_level_sum += self.heightmap[low_point[0]][low_point[1]] + 1
 
         return risk_level_sum
 
-    def ComputePartOneSolution(self, low_points):
-        risk_level = self.CalcRiskLevel(low_points)
-        return risk_level
-
-    def ComputeBasinSizes(self, current_coord, visited_set):
-        # heightmap, current_coord, visited_set
-        # Starts with single pair (low point coords) as input and returns [sum of basin size]
-        # Finds valid adjacent value coordinates
-        
-        # Recursive case: valid adjacent value, within indexes of matrix, --> increments basin_size by one
-        # Base case: adjacent value is a 9, or coordinate exists in basin_coords set
+    def CalcBasinSizes(self, current_coord, visited_set):
+        """
+        Starts with single pair (low point coords) as input, finding valid adjacent value coordinates based on recursive case criteria and returns basin size.
+        Recursive case: valid adjacent value, within indexes of matrix, --> increments basin_size by one.
+        Base case: adjacent value is a 9, or coordinate exists in basin_coords set.
+        """
 
         valid_adjacent_coords = []
         visited_set.add(current_coord)
@@ -109,35 +89,40 @@ class HeightMapAnalyzer():
 
         for adj_coord in valid_adjacent_coords:
             if adj_coord not in visited_set:
-                basin_sum += self.ComputeBasinSizes(adj_coord, visited_set)
+                basin_sum += self.CalcBasinSizes(adj_coord, visited_set)
 
         return basin_sum
 
-    def ComputePartTwoSolution(self):
+    def CalcPartTwoSolution(self, low_points):
         visited_set = set()
-        self.ParseInputFile()
-        low_points = self.ComputeLowPoints()
-
         basin_size_list = []
 
         for low_point_coord in low_points:
-            basin_size_list.append(self.ComputeBasinSizes(low_point_coord, visited_set))
+            basin_size_list.append(self.CalcBasinSizes(low_point_coord, visited_set))
 
         sorted_basin_size_list = sorted(basin_size_list)
-        sum_basin_sizes = sorted_basin_size_list[-1] * sorted_basin_size_list[-2] * sorted_basin_size_list[-3]
+        sum_top_3_basin_sizes = sorted_basin_size_list[-1] * sorted_basin_size_list[-2] * sorted_basin_size_list[-3]
 
-        return sum_basin_sizes
+        return sum_top_3_basin_sizes
 
 
-height_map_analyzer = HeightMapAnalyzer(INPUT_FILENAME)
-height_map_analyzer.ParseInputFile()
+height_map_analyzer = HeightMapAnalyzer()
+
+with open(INPUT_FILENAME) as file:
+    height_map_analyzer.heightmap = []
+    for row in file:
+        stripped_row = row.strip()
+        row_lst = []
+        for column_value in stripped_row:
+            row_lst.append(int(column_value))
+        height_map_analyzer.heightmap.append(row_lst)
+
+low_points = height_map_analyzer.CalcLowPoints()
 
 # # Part 1
-# low_points = height_map_analyzer.ComputeLowPoints()
-# part_one_solution = height_map_analyzer.ComputePartOneSolution(low_points)
-# print(f'Part 1 Solution: {part_one_solution}')
+part_one_solution = height_map_analyzer.CalcPartOneSolution(low_points)
+print(f'Part 1 Solution: {part_one_solution}')
 
 # # Part 2
-# basin_sizes = height_map_analyzer.ComputeBasinSizes()
-part_two_solution = height_map_analyzer.ComputePartTwoSolution()
+part_two_solution = height_map_analyzer.CalcPartTwoSolution(low_points)
 print(f'Part 2 Solution: {part_two_solution}')
