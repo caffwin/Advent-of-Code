ed0851695f0b61f9952fd81a737a8b7e2db5f7ca
caffwin <catheralligator@gmail.com>
2022-07-04 18:27:29 -0700

    day 12 wip

--- 12_passage_pathing/12.py
+++ 12_passage_pathing/12.py
@@ -1,16 +1,43 @@
 # 12 Passage Pathing
+from tkinter import BOTTOM
+from turtle import end_fill
+from typing import Dict, List, Tuple
 
 TEST_FILE_NAME = 'test_input.txt'
 PUZZLE_INPUT_FILE = 'puzzle_input.txt'
 
 
-class Node():
-    def __init__(self, is_big_cave=False, adj_nodes=[]):
-        self.is_big_cave = is_big_cave
-        self.adj_nodes = adj_nodes
+class Node:
+    '''
+    Member variables:
+     - self.value: str
+     - self.neighbors: List[Node]
+    '''
+    def __init__(self, value: str):
+        self.value = value
+        self.neighbors = []
 
-    def return_adj_nodes(self):
-        return self.adj_nodes
+    def return_value(self):
+        return self.value
+
+    def return_neighbors(self):
+        return self.neighbors
+
+def ConnectNodes(node, other_node):
+    node.neighbors.append(other_node)
+    other_node.neighbors.append(node)
+
+def GenerateGraph(adjacent_pairs: List[Tuple[str, str]]) -> Dict[str, Node]:
+    node_dict = {}
+    for left_value, right_value in adjacent_pairs:
+        # Create nodes if missing
+        for value in left_value, right_value:
+            if (value not in node_dict):
+                node_dict[value] = Node(value)
+        # Add connection
+        ConnectNodes(node_dict[left_value], node_dict[right_value])
+    return node_dict
+
 
 def parse_edges_from_input(file_name):
     """
@@ -34,88 +61,62 @@ def parse_edges_from_input(file_name):
 
             edges.append((vertex_one, vertex_two))
 
-    return vertices, edges
+    return edges
 
-def initialize_adj_dict(vertices):
-    adj_dict = {}
-    for vertex in vertices:
-        adj_dict[vertex] = []
-    return adj_dict
-
-def create_adjacency_dict(vertices, edges):
-    adj_dict = initialize_adj_dict(vertices) # A dict containing vertices as keys and empty lists as values
-    for edge in edges:
-        adj_dict[edge[0]].append(edge[1])
-        adj_dict[edge[1]].append(edge[0])
-
-    return adj_dict
-
-def part_one_solution(adj_dict, start_node):
+def dfs_paths_iterative(root_node):
     """
-    Takes in a unique list of vertices and edges (set of tuples) and performs graph traversal (DFS? expand later) to find the total number of
-    possible paths through the cave system (graph). 
-    Lower-case letters represent small caves and can only be traversed through once.
-    Upper-case letters represent large caves that can be traversed through any number of times.
-    Paths must start at start, and end at end.
+    Use Node class
+    Add node to current_path list
+    Check neighbors and set to variable
+
+
+    Keep checking until letter .isupper(), that's the end node
+    Could record end nodes in a list to ignore those neighbours? But seems dumb because xXxtra storage & too contextual
+
+    If length of unvisited neighbours is 0, we've reached 
+    Unvisited neighbors is current node's neighbours minus visited nodes
+
+    Add neighbors to current path
     """
-
-    stack = [start_node] # always initialize to start node
-    invalid_vertices = [] # Includes small caves that have been traversed through already and start vertex
-    num_unique_sequences = 0
-
+    total_paths = 0
+    stack = [root_node]
+    current_path = []
+    visited_nodes = set()
+    
     while len(stack) > 0:
 
-        current_node = stack.pop() # 'start'
-        print('current node: ', current_node)
-        adj_nodes = adj_dict[current_node]
-        print('adj_nodes: ', adj_dict[current_node])
-        if current_node.islower() and current_node != 'end': # Can this even happen if "end" nodes never make it in the stack?  
-            print('adding to invalid_vertices: ', current_node)
-            invalid_vertices.append(current_node)
-        else:
-            print(current_node, 'is a big cave')
+        current_node = stack.pop()
+        current_path.append(current_node)
+        unvisited_neighbors = set(current_node.neighbors) - visited_nodes # a set
 
-        for adj_node in adj_nodes:
-            print('Checking adj nodes and entering for loop.... adj_node is: ', adj_node)
-            if adj_node not in invalid_vertices and adj_node != 'end': # Add node if not invalid
-                print('adding node: ', adj_node)
-                stack.append(adj_node)
+        if current_node.return_value() != 'end': # and current_node.return_value().islower():
+            visited_nodes.add(current_node)
 
-            if adj_node == 'end':
-                num_unique_sequences += 1
-                print('adding one to unique sequences')
+        if current_node.return_value() == 'end': # Behaviour only for end node
+            total_paths += 1
+            current_path.pop() # Remove the end node to make room for other paths
 
-        print('invalid vertices: ', invalid_vertices)
-        print('current stack: ', stack)
+        else: # If not end node, add visitors
+            for neighbor in current_node.neighbors:
+                if neighbor in unvisited_neighbors:
+                    stack.append(neighbor)
+                else:
+                    print('neighbor has been visited: ', neighbor.return_value())
 
-    return num_unique_sequences
+    return total_paths
 
 
 def main():
-    vertices, edges = parse_edges_from_input(TEST_FILE_NAME)
-    adj_dict = create_adjacency_dict(vertices, edges)
+    edges = parse_edges_from_input(TEST_FILE_NAME)
+    graph_adjacent_pairs = edges # [('start', 'A'), ('start', 'b'), ('A', 'b'), ('b', 'c'), ('A', 'c'), ('c', 'end')]
 
-    # test_adj_dict = {
-    #     'a': ['b', 'c'],
-    #     'b': ['d'],
-    #     'c': ['e'],
-    #     'd': ['f'],
-    #     'e': [],
-    #     'f': []
-    # }
-    start_node = 'start' # always starts here
-    print('*** adj_dict: ', adj_dict)
-    print('part one solution: ', part_one_solution(adj_dict, 'start'))
+    node_dict = GenerateGraph(graph_adjacent_pairs)
+    root_node = node_dict['start']
+
+    print(dfs_paths_iterative(root_node))
 
     return
 
 
 if __name__ == "__main__":
     main()
-
-
-# *** adj_dict:  {'start': ['b', 'A'], 'A': ['b', 'end', 'start', 'c'], 'b': ['start', 'A', 'end', 'd'], 'c': ['A'], 'd': ['b'], 'end': ['b', 'A']}
-# *** adj_dict:  {'start': ['b', 'A'], 'A': ['c', 'start', 'b', 'end'], 'b': ['d', 'start', 'end', 'A'], 'c': ['A'], 'd': ['b'], 'end': ['b', 'A']}
-# *** adj_dict:  {'start': ['A', 'b'], 'A': ['b', 'start', 'c', 'end'], 'b': ['A', 'd', 'end', 'start'], 'c': ['A'], 'd': ['b'], 'end': ['b', 'A']} # 5
-# *** adj_dict:  {'start': ['A', 'b'], 'A': ['start', 'b', 'c', 'end'], 'b': ['end', 'd', 'A', 'start'], 'c': ['A'], 'd': ['b'], 'end': ['b', 'A']} # 7
-# *** adj_dict:  {'start': ['b', 'A'], 'A': ['c', 'start', 'end', 'b'], 'b': ['start', 'end', 'd', 'A'], 'c': ['A'], 'd': ['b'], 'end': ['b', 'A']} # 11
