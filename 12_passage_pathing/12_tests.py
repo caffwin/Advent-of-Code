e3a7ba72fb329d1e81b7ab86fb4c066e2b026189
caffwin <catheralligator@gmail.com>
2022-07-10 22:52:34 -0700

    add tests

---
+++ 12_passage_pathing/12_tests.py
@@ -0,0 +1,47 @@
+import day12
+
+TEST_FILE_1_PATH = 'test1.txt'
+TEST_FILE_2_PATH = 'test2.txt'
+TEST_FILE_3_PATH = 'test3.txt'
+
+
+def test_start_end():
+    result = day12.main(TEST_FILE_1_PATH)
+    expected_result = 1
+    print('result: ', result)
+
+    if result == expected_result:
+        print("success test 1")
+    else:
+        print("failure test 1")
+
+def test_all_lower():
+    result = day12.main(TEST_FILE_2_PATH)
+    expected_result = 2
+    print('result: ', result)
+
+    if result == expected_result:
+        return "success test 2"
+    else:
+        return "failure test 2"
+
+def test_casing():
+    result = day12.main(TEST_FILE_3_PATH)
+    expected_result = 5
+    print('result: ', result)
+
+    if result == expected_result:
+        return "success test 2"
+    else:
+        return "failure test 2"
+
+
+
+def main():
+    test_start_end()
+    test_all_lower()
+    test_casing()
+    print('All tests passed!')
+
+if (__name__ == '__main__'):
+    main()
--- 12_passage_pathing/day12.py
+++ 12_passage_pathing/day12.py
@@ -1,7 +1,10 @@
 # 12 Passage Pathing
+from logging import root
+from platform import node
 from typing import Dict, List, Tuple
 
-TEST_FILE_NAME = 'test_input.txt'
+TEST_FILE_NAME = 'test2.txt'
+# TEST_FILE_NAME = 'test_input.txt'
 PUZZLE_INPUT_FILE = 'puzzle_input.txt'
 
 class Node:
@@ -74,7 +77,8 @@ def dfs_paths_iterative_basic(root_node):
     fringe_stack = [(root_node, [])]
     current_path = []
     visited_nodes = set()
-    
+    path_list = []
+
     while len(fringe_stack) > 0:
         current_node, current_path = fringe_stack.pop()
         visited_nodes = current_path
@@ -82,30 +86,24 @@ def dfs_paths_iterative_basic(root_node):
         unvisited_neighbors = set(current_node.neighbors) - set(visited_nodes) 
 
         if current_node.get_value() == 'end':
-            print('*** Reached end, current path: ', [repr(node) for node in current_path])
+            path_list.append([repr(node) for node in current_path])
             total_paths += 1
 
         else:
             for neighbor in current_node.neighbors:
-                if neighbor in unvisited_neighbors or neighbor.get_value().isupper(): # Covers start-A-b-A-end
+                if neighbor in unvisited_neighbors or neighbor.get_value().isupper():
                     fringe_stack.append((neighbor, list(current_path)))
-                else:
-                    print('neighbour has already been visited, not adding: ', neighbor.get_value())
-                
-            if len(unvisited_neighbors) == 0: # When there are no neighbors
-                print("No neighbours left..", [repr(node) for node in current_path])
-                current_path.pop()
 
+    print("total_paths: ", total_paths)
     return total_paths
 
-
-def main():
-    edges = parse_edges_from_input(TEST_FILE_NAME)
+def main(test_file):
+    edges = parse_edges_from_input(test_file)
     graph_adjacent_pairs = edges # [('start', 'A'), ('start', 'b'), ('A', 'b'), ('b', 'c'), ('A', 'c'), ('c', 'end')]
     node_dict = GenerateGraph(graph_adjacent_pairs)
     root_node = node_dict['start']
-    print('basic ver: ', dfs_paths_iterative_basic(root_node))
+    print('result: ', dfs_paths_iterative_basic(root_node))
     return
 
 if __name__ == '__main__':
-    main()
+    main(TEST_FILE_NAME)
