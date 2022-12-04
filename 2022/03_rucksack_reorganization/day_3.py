a166777f508d6a6fc7be3fcd9b4f5c1f6416f837
caffwin <catheralligator@gmail.com>
2022-12-04 01:12:23 -0800

    day 3 part 2 working solution

--- 2022/03_rucksack_reorganization/day_3.py
+++ 2022/03_rucksack_reorganization/day_3.py
@@ -1,5 +1,33 @@
 TEST_INPUT_FILE = 'test_input.txt'
 
+
+def part_two_solution():
+    priority_sum = 0
+    
+    with open(TEST_INPUT_FILE) as file:
+        counter = 0
+        lst_set_compartments = []
+
+        for line in file:
+            stripped_line = line.strip()
+            set_compartment = set(stripped_line)
+            lst_set_compartments.append(set_compartment)
+            counter += 1
+
+            if counter == 3:
+                counter = 0                
+                shared_char = ''.join(set.intersection(*lst_set_compartments))
+
+                if shared_char.islower():
+                    priority_index = ord(shared_char) - 96
+                else: # upper case
+                    priority_index = ord(shared_char) - 38
+
+                lst_set_compartments = []
+                priority_sum += priority_index
+
+    return priority_sum
+
 def part_one_solution():
     priority_sum = 0
     with open(TEST_INPUT_FILE) as file:
@@ -22,6 +50,7 @@ def part_one_solution():
 
 def run():
     print('part one solution: ', part_one_solution())
+    print('part two solution: ', part_two_solution())
 
 if __name__ == "__main__":
     run()
