55629d5638fd3dfe9e1efee25b2c478adf130ca5
caffwin <catheralligator@gmail.com>
2022-12-04 00:31:41 -0800

    day 3 part 1 code cleanup

--- 2022/03_rucksack_reorganization/day_3.py
+++ 2022/03_rucksack_reorganization/day_3.py
@@ -3,15 +3,12 @@ TEST_INPUT_FILE = 'test_input.txt'
 def part_one_solution():
     priority_sum = 0
     with open(TEST_INPUT_FILE) as file:
-        
         for line in file:
-            shared_char = None
             stripped_line = line.strip()
             compartment_one = set(stripped_line[0:int(len(stripped_line)/2)])
             compartment_two = set(stripped_line[int(len(stripped_line)/2):])
 
             shared_char = ''.join(compartment_one.intersection(compartment_two))
-            unicode_char = ord(shared_char)
 
             if shared_char.islower():
                 priority_index = ord(shared_char) - 96
