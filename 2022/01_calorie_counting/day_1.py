5e80671fcb27fc8a618369e6dac0f823e5409c79
caffwin <catheralligator@gmail.com>
2022-12-05 01:02:18 -0800

    code cleanup, improve comments day 1

--- 2022/01_calorie_counting/day_1.py
+++ 2022/01_calorie_counting/day_1.py
@@ -2,7 +2,7 @@ TEST_INPUT_FILE = 'test_input.txt'
 
 def parse_elf_calories_into_dict(TEST_INPUT_FILE):
     """
-        Takes in input file and parses into output dict containing elf (key) and total calories consumed (value).
+        Takes in input file and parses into output dict containing elf number (int) as the key and total calories consumed (int) as the value.
     """
     elf_calorie_dict = {}
     with open(TEST_INPUT_FILE) as file:
@@ -19,18 +19,16 @@ def parse_elf_calories_into_dict(TEST_INPUT_FILE):
 
     return elf_calorie_dict
 
-
 def calc_most_calories_consumed(elf_calorie_dict):
     """
-    Takes in a dictionary and iterates through keys, returns the highest value in the dict (representing number of calories consumed).
+    Takes in elf_calorie_dict (dictionary of elf number (int) to calorie (int) mapping) and iterates through keys, 
+    returning the highest value in the dict. Returns the highest number of calories consumed (value).
     """
-    fattest_elf = None
     most_calories_consumed = 0
 
     for key in elf_calorie_dict:
         if elf_calorie_dict[key] > most_calories_consumed:
             most_calories_consumed = elf_calorie_dict[key]
-            fattest_elf = key
 
     return most_calories_consumed
 
@@ -41,14 +39,13 @@ def calc_sum_top_three_calories_consumed(elf_calorie_dict):
 
     return sum(top_three_results)
 
-
 def run():
     elf_calorie_dict = parse_elf_calories_into_dict(TEST_INPUT_FILE)
     most_calories_consumed = calc_most_calories_consumed(elf_calorie_dict)
-    print('Part 1 answer: ', most_calories_consumed)
+    print('part one solution: ', most_calories_consumed)
 
-    part_2_answer = calc_sum_top_three_calories_consumed(elf_calorie_dict)
-    print('Part 2 answer: ', part_2_answer)
+    part_2_solution = calc_sum_top_three_calories_consumed(elf_calorie_dict)
+    print('part two solution: ', part_2_solution)
 
 if __name__ == "__main__":
     run()
