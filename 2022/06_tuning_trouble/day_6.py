9d35cf33f2218409516f7ca2bcb0e151b28d58e4
caffwin <catheralligator@gmail.com>
2022-12-06 00:55:46 -0800

    day 6 part 2 working solution

--- 2022/06_tuning_trouble/day_6.py
+++ 2022/06_tuning_trouble/day_6.py
@@ -1,5 +1,7 @@
 TEST_FILE_NAME = "test_input.txt"
 DATASTREAM_BUFFER = None
+DISTINCT_NUM_CHARS_P1 = 4
+DISTINCT_NUM_CHARS_P2 = 14
 
 with open(TEST_FILE_NAME) as file:
     for line in file:
@@ -12,21 +14,39 @@ def p1_solution():
     """
     index_flag = False
     packet_marker_index = None
-    for i in range(0, len(DATASTREAM_BUFFER) - 4):
+    for i in range(0, len(DATASTREAM_BUFFER) - DISTINCT_NUM_CHARS_P1):
         packet_string = DATASTREAM_BUFFER[i] + DATASTREAM_BUFFER[i+1] + DATASTREAM_BUFFER[i+2] + DATASTREAM_BUFFER[i + 3]
         set_chars = set(packet_string)
 
-        if len(set_chars) == 4:
+        if len(set_chars) == DISTINCT_NUM_CHARS_P1:
             if not index_flag:
                 packet_marker_index = int(i + 3) + 1
                 index_flag = True
 
     return packet_marker_index
 
+def p2_solution():
+    # 14 distinct characters instead of 4
+    index_flag = False
+    packet_marker_index = None
+    for i in range(0, len(DATASTREAM_BUFFER) - DISTINCT_NUM_CHARS_P2):
+        packet_string = ""
+        for j in range(i, i + DISTINCT_NUM_CHARS_P2):
+            packet_string += DATASTREAM_BUFFER[j]
+            set_chars = set(packet_string)
+            
+            if len(set_chars) == DISTINCT_NUM_CHARS_P2:
+                if not index_flag:
+                    packet_marker_index = j + 1
+                    return packet_marker_index
+
+    return
 
 def run():
     part_one_solution = p1_solution()
+    part_two_solution =  p2_solution()
     print("part one solution: ", part_one_solution)
+    print("part two solution: ", part_two_solution)
     return
 
 if __name__ == "__main__":
