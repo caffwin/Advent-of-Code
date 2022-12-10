f34927ea1fc237efa8e8ea04539016703e2b3637
caffwin <catheralligator@gmail.com>
2022-12-09 18:15:29 -0800

    day 7 part 1 working solution on test example

--- 2022/07_no_space_left_on_device/day_7.py
+++ 2022/07_no_space_left_on_device/day_7.py
@@ -1,78 +1,88 @@
 TEST_FILE_INPUT = "test_input.txt"
-INSTRUCTION_LIST = []
-NODE_LIST = set() # Keep track of all nodes
 
 class Node():
-    def __repr__(self, file_type, children=None) -> str:
-        self.file_type = None
-        self.children = None # By default, no children unless directory -- then pass in list of additional nodes? Maybe.
-        pass
+    def __init__(self, name, file_type=None, file_size=0, children=None, parent=None, total_size=0) -> str:
+        self.name = name
+        self.file_type = file_type
+        self.file_size = file_size 
+        self.children = children # By default, no children unless directory -- then pass in list of additional nodes?
+        self.parent = parent
+        self.total_size = total_size
 
-    def add_children(self, children):
-        self.children.append()
+    def add_children(self, child_node):
+        self.children[child_node.name] = child_node
 
     def show_children(self):
         return self.children
 
+    def print_name(self):
+        print(self.name)
 
-def execute_instruction(current_node, instruction):
+def p1_solution():
+    sum_dir_sizes_under_10k = 0
+    root_node_flag = False
+    current_node = None
 
-    if len(instruction) == 2: # this is an ls -- this helps BUILD the tree
-        print('listing out instructions')
-
-    # Else, it's a cd command -- meaning we traverse in some way
-    else:
-        cd_cmd, move_to = instruction.split(" ")
-        print('moving to file... ', move_to)
-        if move_to == "..": # go up one level
-            print("go up")
-        else:
-            # check neighbours to see if node exists, if not, then 
-            new_node = Node() # pass in parent -- needs to have knowledge of current node
-            NODE_LIST.add(move_to)
-
-def parse_input():
+    # Populate tree as listing out files/dirs
     with open(TEST_FILE_INPUT) as file:
         for line in file:
             stripped_line = line.strip()
+            # if current_node:
+            #     print("current node is: ", current_node.name)
             if stripped_line[0] == "$": # Command
-                command = stripped_line[2:]
-                INSTRUCTION_LIST.append(command)
-            else: # Listing directory -- get file size and name
-                file_size, file_name = stripped_line.split(" ")
-                # print('file size and name: ', file_size, file_name) # Debug
+                output = stripped_line[2:] # can be "cd" or "ls", do nothing for "ls"
+                if output[:2] == "cd": # Ex: "cd a" or "cd .." -- traversing down one level, or up one level
+                    cd_cmd, file_name = output.split(" ") # cd_cmd not used
+                    if file_name == "/": # Root directory, only applicable to line 1
+                        print("** This the root directory! Creating a root node", )
+                        root_node_flag == True
+                        root_node = Node("/", children={})
+                        current_node = root_node
+                        # Root node!
+                    
+                    # Test input never tries to cds into a file.
+                    elif file_name == "..": # Traverse up one level
+                        print("** This is a cd command -- going up one level to: ", current_node.parent.name)
+                        current_node = current_node.parent
+                    else: # down one level, # Node always exists.
+                        print("** This is a cd command -- checking.. ", file_name)
+                        current_node = current_node.children[file_name]
+            else: # Listing directory -- get file size and name. Ex: "29116 f"
+                # A directory and a file can't share the same name.
+                file_type, file_name = stripped_line.split(" ")
+                if file_type == "dir": # probably find better var since "dir" is a type and not size
+                    # If it's a directory, only add if it's 
+                    print("** The file is a directory: ", file_name)
+                    # check current node's neighbours 
+                    if file_name not in current_node.children:
+                        print("**** This is a directory and does not exist in the current node's children, adding new node with name: ", file_name, "to", current_node.name)
+                        new_child_node = Node(file_name, file_type="dir", children={}, parent=current_node) # add children dict because dir
+                        current_node.add_children(new_child_node)
+                    # else: # file, ex "dir e" already exists: do nothing
 
-def p1_solution():
-    # At this point, have a list of instructions!
+                # If the size isn't "dir", then it's always a numerical value greater than 0 
+                # if int(file_size) > 0:
+                else: # Either a directory, or not (it'll be a file starting with a numeric value indicating file_size)
+                    file_size, file_name = stripped_line.split(" ")
+                    
+                    if file_name not in current_node.children: # Check if already a child
+                        new_child_node = Node(file_name, file_type="file", file_size=int(file_size), parent=current_node) # no children
+                        print("** The file is not a directory, it has name: ", file_name, "and size: ", file_size, " adding ", file_name, "to", current_node.name)
+                        current_node.add_children(new_child_node)
+
+def sum_dir_sizes(root_node):
     sum_dir_sizes_under_10k = 0
-
-    # Iterate through each line
-    # First node is always '/', root
-    # Create node, set current_node to that 
-    # ls -- this means populate children, if they don't already exist
-    # Check for children
-
-    # if it's a dir file size, then it can have an array of children
-    # if not dir, call it a "file" and print the size
-
-    # takes in root node (which we already know as '/')
-
-
-
-
-
-
+    list_dir_sizes_under_10k = []
+    # Loop through and sum sizes, then return sum_dir_sizes_under_10k
     return sum_dir_sizes_under_10k
 
 # Determine sizes at a directory level!
 # Find directories with total size less than or equal to 100000 and sum those. What is the answer?
 
 def run():
-    parse_input()
-    # print('instruction list: ', INSTRUCTION_LIST)
-    # for instruction in INSTRUCTION_LIST:
-    #     execute_instruction(instruction)
-    # return
+    print('henlo')
+    part_one_solution = p1_solution()
+    print("p1 solution: ", part_one_solution)
 
 if __name__ == "__main__":
     run()
