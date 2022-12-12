d2b1d5ee06a24eba33789cc8b06eb7c09e90831b
caffwin <catheralligator@gmail.com>
2022-12-11 23:50:06 -0800

    Day 8 part 1 working solution

--- 2022/08_treetop_tree_house/day_8.py
+++ 2022/08_treetop_tree_house/day_8.py
@@ -1,5 +1,5 @@
 TEST_INPUT = "test_input.txt"
-TREE_HEIGHTMAP = [] # 0 shortest, 9 tallest.
+TREE_HEIGHTMAP = [] # 0 shortest, 9 tallest
 
 with open(TEST_INPUT) as file:
     for line in file:
@@ -7,22 +7,40 @@ with open(TEST_INPUT) as file:
         stripped_line = line.strip()
         for num in stripped_line:
             tree_height.append(int(num))
-        TREE_HEIGHTMAP.append(tree_height
-)
+        TREE_HEIGHTMAP.append(tree_height)
 
-def calc_valid_coords(row, col):
+
+def determine_directional_visibility(lst_surrounding_coords, curr_tree_coords, direction):
+    """
+        Checks a single surrounding coord, either in the north, west, east or south direction.
+        Returns False if the tree in that direction is taller than the current tree, else True.
+    """
+    is_visible = True
+
+    for tree_coord in lst_surrounding_coords:
+        if TREE_HEIGHTMAP[tree_coord[0]][tree_coord[1]] >= TREE_HEIGHTMAP[curr_tree_coords[0]][curr_tree_coords[1]]:
+            is_visible = False
+
+    return is_visible
+
+def determine_tree_visibility(row, col):
+    """
+        Takes in a row and column value and checks all coordinates east, west, north and south of those in the heightmap.
+
+        Returns True if either of those flags are True, else False.
+    """
+    
     heightmap_height = len(TREE_HEIGHTMAP)
     heightmap_width = len(TREE_HEIGHTMAP[0])
-
-    coord = (row, col) # tuple of current coord, ex. (3, 2)
-
+    curr_coord = (row, col)
+    
     east_coords = []
     west_coords = []
     north_coords = []
     south_coords = []
 
     # Populate valid east coords
-    for i in range(heightmap_width - (col + 1)): # 11 - 8 = 3, producing i as: 0, 1, 2
+    for i in range(heightmap_width - (col + 1)):
         coord = (row, heightmap_width - i - 1)
         east_coords.append(coord)
 
@@ -41,27 +59,29 @@ def calc_valid_coords(row, col):
         coord = (heightmap_height - i - 1, col)
         south_coords.append(coord)
 
-    return 
+    if determine_directional_visibility(east_coords, curr_coord) or determine_directional_visibility(west_coords, curr_coord) or determine_directional_visibility(north_coords, curr_coord) or determine_directional_visibility(south_coords, curr_coord):
+        return True
+
+    else:
+        return False
 
 def p1_solution(heightmap):
+    total_visible_trees = 0
     heightmap_height = len(heightmap)
     heightmap_width = len(heightmap[0])
     for r in range(heightmap_height):
         row = heightmap[r]
         for c in range(heightmap_width):
-            # tree_height = row[c]
-            col_value = int(row[c])
-            print('current coord is ************* : ', col_value)
-            print('passing coords into function: ', r, c)
-            calc_valid_coords(r, c)
+            is_visible_tree = determine_tree_visibility(r, c)
+            if is_visible_tree:
+                total_visible_trees += 1
 
-    return
+    return total_visible_trees
 
 
 def run():
-    print(TREE_HEIGHTMAP)
-    result = p1_solution(TREE_HEIGHTMAP)
-    print('result: ', result)
+    p1_solution = p1_solution(TREE_HEIGHTMAP)
+    print('part one solution: ', p1_solution)
     return
 
 if __name__ == "__main__":
