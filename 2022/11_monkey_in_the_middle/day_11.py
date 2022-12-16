48e286bab9ac17cfb16b886c75f0b12acbeb4c3b
caffwin <catheralligator@gmail.com>
2022-12-16 01:01:05 -0800

    Day 11 setup

---
+++ 2022/11_monkey_in_the_middle/day_11.py
@@ -0,0 +1,11 @@
+TEST_INPUT = "test_input.txt"
+
+with open(TEST_INPUT) as file:
+    for line in file:
+        stripped_line = line.strip()
+        
+def run():
+    return
+
+if __name__ == "__main__":
+    run()
