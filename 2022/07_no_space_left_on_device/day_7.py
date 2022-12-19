63d6d2a6173b76046054371ddf95c7740457ac7d
caffwin <catheralligator@gmail.com>
2022-12-19 01:47:07 -0800

    Day 7 working solution parts 1 and 2

--- 2022/07_no_space_left_on_device/day_7.py
+++ 2022/07_no_space_left_on_device/day_7.py
@@ -1,12 +1,14 @@
 TEST_FILE_INPUT = "test_input.txt"
 MAX_DIR_SIZE = 100000
+TOTAL_DISK_SPACE_AVAILABLE = 70000000
+REQUIRED_DISK_SPACE = 30000000
 
 class Node():
     def __init__(self, name, file_type=None, file_size=0, children=None, parent=None, total_size=0) -> str:
         self.name = name
         self.file_type = file_type
         self.file_size = file_size 
-        self.children = children # By default, no children unless directory -- then pass in list of additional nodes?
+        self.children = children
         self.parent = parent
         self.total_size = total_size
 
@@ -27,33 +29,25 @@ def p1_solution():
     with open(TEST_FILE_INPUT) as file:
         for line in file:
             stripped_line = line.strip()
-            # if current_node:
-            #     print("current node is: ", current_node.name)
             if stripped_line[0] == "$": # Command
                 output = stripped_line[2:] # can be "cd" or "ls", do nothing for "ls"
                 if output[:2] == "cd": # Ex: "cd a" or "cd .." -- traversing down one level, or up one level
                     cd_cmd, file_name = output.split(" ") # cd_cmd not used
                     if file_name == "/": # Root directory, only applicable to line 1
-                        print("** This the root directory! Creating a root node", )
-                        root_node = Node("/", children={})
+                        root_node = Node("/", file_type="dir", children={})
                         current_node = root_node
                         root_node_placeholder = root_node
                     
                     # Test input never tries to cds into a file.
                     elif file_name == "..": # Traverse up one level
-                        # print("** This is a cd command -- going up one level to: ", current_node.parent.name)
                         current_node = current_node.parent
-                    else: # down one level, # Node always exists.
-                        # print("** This is a cd command -- checking.. ", file_name)
+                    else: # down one level, assumption is that Node always exists.
                         current_node = current_node.children[file_name]
             else: # Listing either file or dir
                 # A directory and a file can't share the same name.
                 file_type, file_name = stripped_line.split(" ")
                 if file_type == "dir": # A directory - probably find better var since "dir" is a type and not size
-                    # print("** The file is a directory: ", file_name)
-                    # If it's a directory, only add if it's not a child
                     if file_name not in current_node.children:
-                        # print("**** This is a directory and does not exist in the current node's children, adding new node with name: ", file_name, "to", current_node.name)
                         new_child_node = Node(file_name, file_type="dir", children={}, parent=current_node) # add children dict because dir
                         current_node.file_size += new_child_node.file_size
                         current_node.add_children(new_child_node)
@@ -64,56 +58,65 @@ def p1_solution():
                     if file_name not in current_node.children: # Check if already a child
                         new_child_node = Node(file_name, file_type="file", file_size=int(file_size), parent=current_node) # no children
                         current_node.file_size += new_child_node.file_size
-                        # print("** The file is not a directory, it has name: ", file_name, "and size: ", file_size, " adding ", file_name, "to", current_node.name)
                         current_node.add_children(new_child_node)
 
+    calc_sum_dir_sizes_recursive(root_node_placeholder) # Populate directory sizes
     sum = sum_dir_sizes_under_10k(root_node_placeholder)
+    print("Part two solution: ", part_two_solution(root_node_placeholder))
     return sum
 
-def sum_dir_sizes_under_10k(root_node):
-    # Try DFS, only regarding directories.
-    fringe_stack = [root_node] 
-    sum_dir_sizes_under_10k = 0
-    dir_size_list = []
-    visited_list = []
-    # Can store a tuple of (name, size)
-    current_node = None
-    while len(fringe_stack) > 0:
-        current_node = fringe_stack.pop(0)
-        print('current node name: ', current_node.name)
-        print('current node: ', current_node.show_children())
-        # total_size_current_node = current_node.file_size
-        # print('file size of node: ', total_size_current_node)
-        for child in current_node.children: # child is a string
-            child_node = current_node.children[child]
-            if child_node.file_type == "dir" and child_node.name not in visited_list: # current_node.children[child] returns the value, which is the node
-                # itzOnlyDirectoriez
-                # total_size_current_node += child_node.file_size
-                fringe_stack.append(child_node)
-                # If it's a directory, not in visited list, then add sum
-                dir_tuple = (child_node.name, child_node.file_size)
-                dir_size_list.append(dir_tuple)
-                print('Directory child node found! dir name: ', child_node.name, " and size: ", str(child_node.file_size))
-                # dir_tuple = (child_node.name, child_node.file_size)
-                # dir_size_list.append(dir_tuple)
-                # add child node to parent node's size
 
+# Recursive
+def calc_sum_dir_sizes_recursive(node):
+    if not node.children or node.children == 0: # Regular file
+        return node.file_size
+    else: # Directory
+        size = 0
+        for child in node.children:
+            child_node = node.children[child]
+            size += calc_sum_dir_sizes_recursive(child_node)
 
-    for dir_size_pair in dir_size_list:
-        if dir_size_pair[1] <= MAX_DIR_SIZE:
-            sum_dir_sizes_under_10k += dir_size_pair[1]
-    # Loop through and sum sizes, then return sum_dir_sizes_under_10k
-    # Traverse through entire `tree`
-    print("dir_size_list: ", dir_size_list)
-    return sum_dir_sizes_under_10k
+        node.file_size = size
+        return size
 
-# Determine sizes at a directory level!
-# Find directories with total size less than or equal to 100000 and sum those. What is the answer?
+# Iterate through nodes
+def sum_dir_sizes_under_10k(node):
+    fringe_stack = [node]
+    sum_dirs_under_100k = 0
+    while fringe_stack:
+        current_node = fringe_stack.pop()
+        # print("%s %d %s" % (current_node.name, current_node.file_size, current_node.file_type)) # Debug
+        if current_node.file_type == "dir":
+            for child in current_node.children:
+                child_node = current_node.children[child]
+                if child_node.file_type == "dir":
+                    fringe_stack.append(child_node)
+            if current_node.file_size <= 100000:
+                sum_dirs_under_100k += current_node.file_size
+    return sum_dirs_under_100k
+
+def part_two_solution(root_node):
+    total_tree_file_size = root_node.file_size
+    current_unused_space = TOTAL_DISK_SPACE_AVAILABLE - total_tree_file_size
+    need_to_free = REQUIRED_DISK_SPACE - current_unused_space
+    fringe_stack = [root_node]
+    closest_file_size = float('inf')
+
+    while fringe_stack:
+        current_node = fringe_stack.pop()
+        # print("%s %d %s" % (current_node.name, current_node.file_size, current_node.file_type))
+        if current_node.file_type == "dir":
+            for child in current_node.children:
+                child_node = current_node.children[child]
+                if child_node.file_type == "dir":
+                    fringe_stack.append(child_node)
+            if current_node.file_size >= need_to_free:
+                closest_file_size = min(closest_file_size, current_node.file_size)
+    return closest_file_size
 
 def run():
-    print('henlo')
     part_one_solution = p1_solution()
-    print("p1 solution: ", part_one_solution)
+    print("Part one solution: ", part_one_solution)
 
 if __name__ == "__main__":
     run()
